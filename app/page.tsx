6e8b6c01d22ec6a27db116c5c58fa758aed46eb1
ayush143-maker <rahulbhartimzp7@gmail.com>
2026-08-20 10:28:35 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-import PaperTear from "@/components/PaperTear";
-import Hero from "@/components/Hero";
-import Story from "@/components/Story";
-import Turn from "@/components/Turn";
-import Projects from "@/components/Projects";
-import Stack from "@/components/Stack";
-import FileTree from "@/components/FileTree";
-import AboutNote from "@/components/AboutNote";
-import Contact from "@/components/Contact";
+import PaperTear from "../components/PaperTear";
+import Hero from "../components/Hero";
+import Story from "../components/Story";
+import Turn from "../components/Turn";
+import Projects from "../components/Projects";
+import Stack from "../components/Stack";
+import FileTree from "../components/FileTree";
+import AboutNote from "../components/AboutNote";
+import Contact from "../components/Contact";
 
 export default function Home() {
   return (
